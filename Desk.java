2788c217f44a6fbd8de8cf5c68a8431cf8170a5d
Taimoor Abrar <64287217+taimoorabrar22@users.noreply.github.com>
2021-03-27 13:49:21 -0600

    Add files via upload
    class files for project

---
+++ Desk.java
@@ -0,0 +1,31 @@
+package edu.ucalgary.ensf409;
+
+class Desk {
+    private final String ID;
+	private final String Type;
+	private final String Legs;
+	private final String Top;
+    private final String Drawer;
+    private final int Price;
+    private final String ManuID;
+
+    public String getID() {
+        return ID;
+    }
+    public String getType() {
+        return Type;
+    }
+    public String getLegs() {
+        return Legs;
+    }
+    public String getTop() { return Top; }
+    public String getDrawer() {
+        return Drawer;
+    }
+    public int getPrice(){return Price};
+    public int getManuID() {
+        return ManuID;
+    }
+    
+}
+
