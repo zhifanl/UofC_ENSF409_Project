b84a64cdb126a67a622226db670a5f613170af97
3010-manp <81481171+3010-manp@users.noreply.github.com>
2021-03-28 19:57:32 +0530

    Update Manufacturer.java

--- edu/ucalgary/ensf409/Manufacturer.java
+++ edu/ucalgary/ensf409/Manufacturer.java
@@ -6,35 +6,52 @@ class Manufacturer {
     private String Name;
     private String Phone;
     private String Province;
-
+    /*
+    * Set the String value to ManuID
+    */
     public void setManuID(String manuID) {
 		ManuID = manuID;
 	}
-
+    /*
+    * Set the String value to Name
+    */
 	public void setName(String name) {
 		Name = name;
 	}
 
+    /*
+    * Set the String value to Phone
+    */
 	public void setPhone(String phone) {
 		Phone = phone;
 	}
-
+    /*
+    * Set the String value to Province
+    */
 	public void setProvince(String province) {
 		Province = province;
 	}
-
+    /*
+    * returns ManuID
+    */
 	public String getManuID() {
         return ManuID;
     }
-
+    /*
+    * returns Name
+    */
     public String getName() {
         return Name;
     }
-
+    /*
+    * returns Phone
+    */
     public String getPhone() {
         return Phone;
     }
-
+    /*
+    * returns Province
+    */
     public String getProvince() {
         return Province;
     }
