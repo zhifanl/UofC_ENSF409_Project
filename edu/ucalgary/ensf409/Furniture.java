02205a6dcf5437c23ba48c99d023da4e971244b2
3010-manp <81481171+3010-manp@users.noreply.github.com>
2021-03-28 20:06:22 +0530

    Update Furniture.java

--- edu/ucalgary/ensf409/Furniture.java
+++ edu/ucalgary/ensf409/Furniture.java
@@ -6,34 +6,51 @@ public abstract class Furniture {
     private int Price;
     private String ManuID;
 
+    /*
+    * returns ManuID
+    */
     public String getManuID() {
         return ManuID;
     }
-
+    /*
+    * returns ID
+    */
     public String getID() {
         return ID;
     }
-
+    /*
+    * returns Type
+    */
     public String getType() {
         return Type;
     }
-
+    /*
+    * returns Price
+    */
     public int getPrice(){
     	return Price;
     }
-
+    /*
+    * Set the String value to ID
+    */
 	public void setID(String iD) {
 		ID = iD;
 	}
-
+    /*
+    * Set the String value to Type
+    */
 	public void setType(String type) {
 		Type = type;
 	}
-
+    /*
+    * Set the String value to Price
+    */
 	public void setPrice(int price) {
 		Price = price;
 	}
-
+    /*
+    * Set the String value to ManuID
+    */
 	public void setManuID(String manuID) {
 		ManuID = manuID;
 	}
