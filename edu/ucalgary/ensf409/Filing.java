3ee0b7c4641910ea2802294fba2698a1c50c9ced
3010-manp <81481171+3010-manp@users.noreply.github.com>
2021-04-03 08:35:13 -0600

    Update Filing.java

--- edu/ucalgary/ensf409/Filing.java
+++ edu/ucalgary/ensf409/Filing.java
@@ -6,18 +6,31 @@ class Filing extends Furniture{
 	private boolean Rails;
 	private boolean Drawers;
 	private boolean Cabinet;
-
+	
+	/*
+	* returns Rails
+	*/
 	public boolean getRails() {
 		return Rails;
 	}
-
+	
+	/*
+	* returns Drawers
+	*/
 	public boolean getDrawers() {
 		return Drawers;
 	}
-
+	
+	/*
+	* returns Cabinet
+	*/
 	public boolean getCabinet() {
 		return Cabinet;
 	}
+	/*
+	*Receievs the String rails as an argument and if rails equals Y
+	*Then, set the field Rails to true else set the field Rails to false
+	*/
 
 	public void setRails(String rails) {
 		if(rails.equals("Y")) {
@@ -27,6 +40,10 @@ class Filing extends Furniture{
 			this.Rails = false;
 		}
 	}
+	/*
+	*Receievs the String drawers as an argument and if drawers equals Y
+	*Then, set the field Drawers to true else set the field Drawers to false
+	*/
 
 	public void setDrawers(String drawers) {
 		if(drawers.equals("Y")) {
@@ -36,6 +53,10 @@ class Filing extends Furniture{
 			this.Drawers = false;
 		}
 	}
+	/*
+	*Receievs the String cabinet as an argument and if cabinet equals Y
+	*Then, set the field Cabinet to true else set the field Cabinet to false
+	*/
 
 	public void setCabinet(String cabinet) {
 		if(cabinet.equals("Y")) {
