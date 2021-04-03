92db511f6b9a6cadf2ac4b5511d0e663f61b4676
3010-manp <81481171+3010-manp@users.noreply.github.com>
2021-04-03 08:30:17 -0600

    Update Desk.java

--- edu/ucalgary/ensf409/Desk.java
+++ edu/ucalgary/ensf409/Desk.java
@@ -9,26 +9,44 @@ class Desk extends Furniture{
 
 	private int Price;
 	private String ManuID;
-
+	
+	/*
+	* returns Legs
+	*/
 	public boolean getLegs() {
 		return Legs;
 	}
-
+	
+	/*
+	* returns Top
+	*/
 	public boolean getTop() {
 		return Top;
 	}
-
+	/*
+	* returns Drawer
+	*/
 	public boolean getDrawer() {
 		return Drawer;
 	}
-
+	
+	/*
+	* returns Price
+	*/
 	public int getPrice(){
 		return Price;
 	}
-
+	
+	/*
+	* returns ManuID
+	*/
 	public String getManuID() {
 		return ManuID;
 	}
+	/*
+	*Receievs the Strings legs as an argument and if legs equals Y
+	*Then, set the field Legs to true else set the field Legs to false
+	*/
 
 	public void setLegs(String legs) {
 		if(legs.equals("Y")) {
@@ -38,6 +56,10 @@ class Desk extends Furniture{
 			this.Legs = false;
 		}
 	}
+	/*
+	*Receievs the Strings top as an argument and if top equals Y
+	*Then, set the field Top to true else set the field Top to false
+	*/
 
 	public void setTop(String top) {
 		if(top.equals("Y")) {
@@ -47,6 +69,10 @@ class Desk extends Furniture{
 			this.Top = false;
 		}
 	}
+	/*
+	*Receievs the Strings drawer as an argument and if drawer equals Y
+	*Then, set the field Drawer to true else set the field Drawer to false
+	*/
 
 	public void setDrawer(String drawer) {
 		if(drawer.equals("Y")) {
@@ -56,10 +82,16 @@ class Desk extends Furniture{
 			this.Drawer = false;
 		}
 	}
+	/*
+	 * Set the String value to Price
+	 */
 
 	public void setPrice(int price) {
 		Price = price;
 	}
+	/*
+	 * Set the String value to ManuID
+	 */
 
 	public void setManuID(String manuID) {
 		ManuID = manuID;
